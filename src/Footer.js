35a11f23bd5297ffcc6b3bc742e508bff38baaff
Akash Maurya <47055948+akash99909@users.noreply.github.com>
2021-06-03 01:33:30 +0530

    Details Added

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>© 2021 Airbnb clone! !</p>
+            <p>Cloned by akash99909</p>
+        </div>
+    )
+}
+
+export default Footer
